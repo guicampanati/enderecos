70e8024b581832080c62bbe2a87a153bfc0a631d
Guilherme <guicampanati@gmail.com>
2020-01-27 11:09:37 -0300

    Aprimorando teste formulário

--- src/components/__tests__/form.test.js
+++ src/components/__tests__/form.test.js
@@ -29,12 +29,69 @@ describe('<Form />', () => {
     );
   });
 
-  test('should call onSubmit', async () => {
+  test('should render save button and change disabled attribute', async () => {
+    const saveButton = component.container.querySelector('button');
+    const cepInput = component.getByLabelText('CEP');
+
+    expect(saveButton).toHaveTextContent('Salvar');
+    expect(saveButton).toHaveAttribute('disabled');
+
+    fireEvent.change(cepInput, { target: { value: '01001000' } });
+
+    await waitForElement(() => component.container.querySelector('form'));
+
+    expect(saveButton).not.toHaveAttribute('disabled');
+  });
+
+  test('should render undo button and change disabled attribute', async () => {
+    const undoButton = component.container.querySelector('button:nth-child(2)');
+    const cepInput = component.getByLabelText('CEP');
+
+    expect(undoButton).toHaveTextContent('Desfazer');
+    expect(undoButton).toHaveAttribute('disabled');
+
+    fireEvent.change(cepInput, { target: { value: '01001000' } });
+
+    await waitForElement(() => component.container.querySelector('form'));
+
+    expect(undoButton).not.toHaveAttribute('disabled');
+  });
+
+  test('should render error message with empty data onSubmit', async () => {
+    const form = component.container.querySelector('form');
+    const cepInput = component.getByLabelText('CEP');
+
+    fireEvent.change(cepInput, { target: { value: '' } });
+    fireEvent.submit(form);
+
+    await waitForElement(() => component.container.querySelector('form'));
+
+    expect(onSubmit.mock.calls.length).toBe(0);
+    expect(component.container.querySelector('p')).toHaveTextContent(
+      'Campo Obrigatório.'
+    );
+  });
+
+  test('should render error message with invalid data onSubmit', async () => {
+    const form = component.container.querySelector('form');
+    const cepInput = component.getByLabelText('CEP');
+
+    fireEvent.change(cepInput, { target: { value: '999' } });
+    fireEvent.submit(form);
+
+    await waitForElement(() => component.container.querySelector('form'));
+
+    expect(onSubmit.mock.calls.length).toBe(0);
+    expect(component.container.querySelector('p')).toHaveTextContent(
+      'CEP Inválido.'
+    );
+  });
+
+  test('should call onSubmit with valid data', async () => {
     const form = component.container.querySelector('form');
     const cepInput = component.getByLabelText('CEP');
     const numInput = component.getByLabelText('Número');
 
-    // valid data
     fireEvent.change(cepInput, { target: { value: '01001000' } });
     fireEvent.change(numInput, { target: { value: '101' } });
     fireEvent.submit(form);

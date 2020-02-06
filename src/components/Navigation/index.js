5a06b3973525a49fac090106d6b1435ad39f369f
Guilherme <guicampanati@gmail.com>
2020-02-06 18:46:30 -0300

    Comportamento Header responsivo

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,106 +1,169 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-import CreateAddress from './CreateAddress';
 import Logo from '../../svg/Logo';
 import Hamburger from '../../svg/Hamburger';
 import Close from '../../svg/Close';
+import { useWindowSize } from '../../hooks';
 
 const Navigation = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const size = useWindowSize();
 
   return (
-    <Header>
-      <Div>
-        <LinkLogo to={'/'}>
-          <Logo />
-          <span>Endereços</span>
-        </LinkLogo>
-        <Menu>
-          <Button type="button" onClick={() => setIsOpen(!isOpen)}>
-            {isOpen ? <Close /> : <Hamburger />}
-          </Button>
-        </Menu>
-      </Div>
-      {isOpen && (
-        <Nav>
-          <CreateAddress />
-        </Nav>
+    <Nav>
+      <LinkLogo to={'/'}>
+        <Logo />
+        <span>Endereços</span>
+      </LinkLogo>
+
+      <HamburgerMenu>
+        <HamburgerButton type="button" onClick={() => setIsOpen(!isOpen)}>
+          {isOpen ? <Close /> : <Hamburger />}
+        </HamburgerButton>
+      </HamburgerMenu>
+
+      {(size[0] > 640 || isOpen) && (
+        <>
+          <Menu>
+            <MenuItems>
+              <Link to={'/'}>Listar Endereços</Link>
+            </MenuItems>
+          </Menu>
+
+          <div>
+            <LinkCreate to="/criar">Adicionar Endereço</LinkCreate>
+          </div>
+        </>
       )}
-    </Header>
+    </Nav>
   );
 };
 
-const Header = styled.header`
-  background-color: ${props => props.theme.color.gray.dark};
-  padding: ${props => props.theme.spacing.s3};
-
-  @media (min-width: ${props => props.theme.screen.sm}) {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  }
-`;
-
-const Div = styled.div`
+const Nav = styled.nav`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  padding: ${props => props.theme.spacing.s3};
-
-  @media (min-width: ${props => props.theme.screen.sm}) {
-    padding: 0;
-  }
-`;
-
-const Menu = styled.div`
-  @media (min-width: ${props => props.theme.screen.sm}) {
-    display: none;
-  }
-
-  svg {
-    width: ${props => props.theme.spacing.s6};
-    height: ${props => props.theme.spacing.s6};
-    fill: currentColor;
-  }
-`;
-
-const Button = styled.div`
-  display: block;
-  color: ${props => props.theme.color.gray.light};
-
-  &:hover {
-    color: ${props => props.theme.color.white};
-  }
-
-  &:focus {
-    color: ${props => props.theme.color.white};
-    outline: none;
-  }
-`;
-
-const Nav = styled.nav`
-  @media (min-width: ${props => props.theme.screen.sm}) {
-    display: none;
-  }
+  flex-wrap: wrap;
+  background-color: ${props => props.theme.color.gray.dark};
+  padding: ${props => props.theme.spacing.s6};
 `;
 
 const LinkLogo = styled(Link)`
   display: flex;
   align-items: center;
-  font-weight: ${props => props.theme.fontWeight.bold};
+  flex-shrink: 0;
+  font-weight: ${props => props.theme.fontWeight.semibold};
   font-size: ${props => props.theme.fontSize.xl3};
   color: ${props => props.theme.color.gray.light};
   fill: ${props => props.theme.color.gray.light};
   text-decoration: none;
+  letter-spacing: -0.025em;
+  margin-right: ${props => props.theme.spacing.s6};
 
   svg {
+    width: ${props => props.theme.spacing.s8};
+    height: ${props => props.theme.spacing.s8};
     margin-right: ${props => props.theme.spacing.s3};
+    fill: currentColor;
   }
 
   &:hover {
     color: ${props => props.theme.color.white};
     fill: ${props => props.theme.color.white};
+  }
+`;
+
+const HamburgerMenu = styled.div`
+  display: block;
+
+  @media (min-width: ${props => props.theme.screen.sm}) {
+    display: none;
+  }
+`;
+
+const HamburgerButton = styled.button`
+  display: flex;
+  align-items: center;
+  padding: ${props => props.theme.spacing.s2};
+  color: ${props => props.theme.color.gray.light};
+  background-color: ${props => props.theme.color.gray.dark};
+  border-radius: ${props => props.theme.borderRadius.default};
+  border-color: ${props => props.theme.color.gray.light};
+  border-width: 1px;
+
+  &:hover {
+    color: ${props => props.theme.color.white};
+    border-color: ${props => props.theme.color.white};
+  }
+
+  svg {
+    width: ${props => props.theme.spacing.s5};
+    height: ${props => props.theme.spacing.s5};
+    fill: currentColor;
+  }
+`;
+
+const Menu = styled.div`
+  width: ${props => props.theme.width.full};
+  display: block;
+  flex-grow: 1;
+
+  @media (min-width: ${props => props.theme.screen.sm}) {
+    display: flex;
+    align-items: center;
+    width: auto;
+  }
+`;
+
+const MenuItems = styled.div`
+  font-size: ${props => props.theme.fontSize.sm};
+
+  @media (min-width: ${props => props.theme.screen.sm}) {
+    flex-grow: 1;
+  }
+
+  a {
+    display: block;
+    margin-top: ${props => props.theme.spacing.s4};
+    margin-right: ${props => props.theme.spacing.s4};
+    color: ${props => props.theme.color.gray.light};
+
+    @media (min-width: ${props => props.theme.screen.sm}) {
+      display: inline-block;
+      margin-top: 0;
+    }
+
+    &:hover {
+      color: ${props => props.theme.color.white};
+    }
+  }
+`;
+
+const LinkCreate = styled(Link)`
+  display: inline-block;
+  font-size: ${props => props.theme.fontSize.sm};
+  padding: ${props => props.theme.spacing.s2};
+  line-height: 1;
+  border-radius: ${props => props.theme.borderRadius.default};
+  border-color: ${props => props.theme.color.gray.light};
+  border-width: 1px;
+  color: ${props => props.theme.color.gray.light};
+  margin-top: ${props => props.theme.spacing.s4};
+
+  @media (min-width: ${props => props.theme.screen.sm}) {
+    margin-top: 0;
+  }
+
+  &:hover {
+    background-color: ${props => props.theme.color.gray.light};
+    color: ${props => props.theme.color.gray.dark};
+    border-color: transparent;
+  }
+
+  svg {
+    fill: currentColor;
+    margin-right: ${props => props.theme.spacing.s2};
   }
 `;
 

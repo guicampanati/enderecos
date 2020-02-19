03a9633865988424c38fedadc290b0c98513439b
Guilherme <guicampanati@gmail.com>
2020-02-19 15:21:15 -0300

    Teste de unidade Delete

---
+++ src/pages/__tests__/delete.test.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { render } from '../../test-utils';
+import Delete from '../Delete';
+
+describe('<Delete />', () => {
+  let component;
+  let modal = document.createElement('div');
+  modal.setAttribute('id', 'modal');
+
+  beforeEach(() => {
+    component = render(<Delete />, {
+      container: document.body.appendChild(modal)
+    });
+  });
+
+  test('should render delete message', () => {
+    const message = component.container.querySelector('p');
+
+    expect(message).toHaveTextContent(
+      'Tem certeza que quer deletar este endereço?'
+    );
+  });
+
+  test('should render delete button', () => {
+    const deleteButton = component.container.querySelector('a');
+    expect(deleteButton).toHaveTextContent('Deletar');
+    expect(deleteButton).toHaveAttribute('danger');
+    expect(deleteButton).toHaveAttribute('href', expect.stringMatching('/'));
+  });
+
+  test('should render cancel button', () => {
+    const cancelButton = component.container.querySelector('a:nth-child(2)');
+    expect(cancelButton).toHaveTextContent('Cancelar');
+    expect(cancelButton).toHaveAttribute('href', expect.stringMatching('/'));
+  });
+});

c82cfd9d2a0aedb0739c7770d32292b85927313b
guicampanati <50670990+guicampanati@users.noreply.github.com>
2020-02-22 00:03:39 +0000

    Deploying to gh-pages from  - c1dd5eade258d56e03b930f9c66e0656acfe7386 🚀

---
+++ precache-manifest.48cb8b8b55746bb5a98f03d82e07b8b8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cac129672d15694db7fa2e873ed84f56",
+    "url": "/enderecos/index.html"
+  },
+  {
+    "revision": "1ca835c22e9f8a27f214",
+    "url": "/enderecos/static/css/main.6529d1c1.chunk.css"
+  },
+  {
+    "revision": "c9331365893e7d01c98c",
+    "url": "/enderecos/static/js/2.29d8d6fd.chunk.js"
+  },
+  {
+    "revision": "0bdc4f0d8b3d84ae6a73bb9902b479ee",
+    "url": "/enderecos/static/js/2.29d8d6fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ca835c22e9f8a27f214",
+    "url": "/enderecos/static/js/main.0cc0e651.chunk.js"
+  },
+  {
+    "revision": "4a4b381fbe8653fd8ed5",
+    "url": "/enderecos/static/js/runtime-main.478d182a.js"
+  },
+  {
+    "revision": "3d73387c2a1e21f207a8357724f536ba",
+    "url": "/enderecos/static/media/owfont-regular.3d73387c.ttf"
+  },
+  {
+    "revision": "e00c269cd4fb37b24eea2e4f37982c3d",
+    "url": "/enderecos/static/media/owfont-regular.e00c269c.eot"
+  },
+  {
+    "revision": "ed668bf0b123371be46d005b428f6188",
+    "url": "/enderecos/static/media/owfont-regular.ed668bf0.woff"
+  },
+  {
+    "revision": "f3ce0ef136eaaf2b1db22cf4069cb5dc",
+    "url": "/enderecos/static/media/owfont-regular.f3ce0ef1.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/enderecos/precache-manifest.3fa5ab895b66d38a31d227c08bfe61ef.js"
+  "/enderecos/precache-manifest.48cb8b8b55746bb5a98f03d82e07b8b8.js"
 );
 
 self.addEventListener('message', (event) => {
